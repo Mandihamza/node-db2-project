7eef16f58ed45b620ac36af72d3dbdede5cdd6ea
mandihamza <mandihamza@gmail.com>
2020-02-29 15:24:10 -0700

    Adds delete route for cars db

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -57,4 +57,13 @@ router.put("/:id", async (req, res, next) => {
     }
 })
 
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db("cars").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router
