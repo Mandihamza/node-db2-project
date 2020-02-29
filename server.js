d98ac1b0fe7050088214872d603d5776b5e56d0d
mandihamza <mandihamza@gmail.com>
2020-02-29 13:21:22 -0700

    Imports carsRouter

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const carsRouter = require("./cars/carsRouter.js")
 const welcomeRouter = require("./welcome/welcomeRouter.js")
 
 const server = express();
 
 server.use(express.json());
 server.use("/", welcomeRouter)
+server.use("/api/cars", carsRouter)
 
 server.use((err, req, res, next) => {
 	console.log(err)
